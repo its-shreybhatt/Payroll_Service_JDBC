790d49026fbdfc5898acfeeef4fca339a006895f
its-shreybhatt <shreybhatt2@gmail.com>
2023-04-10 13:43:41 +0530

    Welcome to Payroll Service Using JDBC

---
+++ src/main/java/org/example/OS.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class OS {
+    public static String user="root";
+    public static String password="Rahulpanwar@12";
+    public static String url= "jdbc:mysql://localhost:3306";
+}
