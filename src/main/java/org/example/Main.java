6166356a6e03a018fc6d3ac69877a05c894cd3a1
its-shreybhatt <shreybhatt2@gmail.com>
2023-04-10 13:59:58 +0530

    Added : Code to create a payroll service

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,35 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("Welcome to Payroll Service Using JDBC");
+
+        Connection connection = DriverManager.getConnection(OS.url, OS.user, OS.password);
+        Statement statement = connection.createStatement();
+
+        String createDatabase = "CREATE DATABASE payroll_service_JDBC";
+        String useDatabase = "USE payroll_service_JDBC";
+
+        statement.execute(createDatabase);
+        statement.execute(useDatabase);
+
+
+        String deleteDatabase = "DROP DATABASE payroll_service_JDBC";
+
+
+        System.out.print("Do you want to delete the Table (Yes/No) - ");
+        String choice = new Scanner(System.in).next();
+
+        if (choice.equalsIgnoreCase("yes")) {
+            statement.execute(deleteDatabase);
+            System.out.println("Tables deleted successfully....");
+        }
 
     }
 }
